5b033f79ec4df0622590099a75558c3ee5264ec3
James Garbutt <43081j@users.noreply.github.com>
2025-09-25 22:58:31 +0100

    docs: update readme example (#11)
    * docs: update readme example
    
    Fixes #9
    
    * chore: update formatBytes

--- main.js
+++ main.js
@@ -24198,8 +24198,8 @@ async function calculateTotalDependencySizeIncrease(newVersions) {
 // src/main.ts
 function formatBytes(bytes) {
   if (bytes === 0) return "0 B";
-  const k = 1024;
-  const sizes = ["B", "KB", "MB", "GB"];
+  const k = 1e3;
+  const sizes = ["B", "kB", "MB", "GB"];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(1))} ${sizes[i]}`;
 }
